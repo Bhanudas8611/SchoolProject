b0b6e8cbb0a484479b6343c85c596ac1dd3121d6
Bhanudas8611 <bhanudaschattar8611@gmail.com>
2021-10-13 15:42:57 +0530

    create Validation

--- Controller/student.controller.js
+++ Controller/student.controller.js
@@ -1,6 +1,8 @@
-const { query } = require('express');
+const express = require('express');
 const connection=require('./mysql.connection');
 
+
+//Show all Student Data
 let getAllStudentData=(req,res,next)=>{
     connection.query(`SELECT * FROM student`,(err,result)=>{
         if(err)console.log(err);
@@ -10,28 +12,59 @@ let getAllStudentData=(req,res,next)=>{
 
 }
 
+//Find Student details by student id
 let getStudentByID=(req,res,next)=>{
     connection.query(`SELECT Id, Name, Email, Age, Mobile, password FROM student WHERE Id='${req.params.id}'`,(err,result)=>{
         if(err){console.log(err);}
         else {res.send({error:false,data:result});}
     })
+}
 
+//Insert Record in student Table
     let insertStudent=(req,res,next)=>{
         let Name=req.body.Name;
         let Email=req.body.Email;
         let Age=req.body.Age;
         let Mobile=req.body.Mobile;
-        connection.query(`INSERT INTO student(Name, Email, Age, Mobile) VALUES ('${Name}','${Name}','${Age}','${Mobile}')`,(err,result)=>{
+        connection.query(`INSERT INTO student(Name, Email, Age, Mobile) VALUES ('${Name}','${Email}','${Age}','${Mobile}')`,(err,result)=>{
             if(err){console.log(err);}
             else {res.send({error:false,data:result});}
         })
+    }
+
+    //Update Student details by using student ID
+    let updateStudent=(req,res,next)=>{
+        
+        let Email=req.body.Email;
+        let Age=req.body.Age;
+        let Mobile=req.body.Mobile;
+        connection.query(`UPDATE student SET Email='${Email}',Age='${Age}',Mobile='${Mobile}' WHERE Id=${req.body.id}`,(err,result)=>{
+            if(err){console.log(err);}
+            else if(result.affectedRows>0)
+            {
+                res.send({error:false, message:'Record Updated', data:result[0]})
+            }
+                      
+        })
+    }
+
+    let deleteStudent=(req,res,next)=>{
+         connection.query(`DELETE FROM student WHERE Id=${req.params.id}`,(err,result)=>{
+            if(err){console.log(err);}
+            else if(result.affectedRows>0)
+            {
+                res.send({error:false, message:'Record Deleted', data:result[0]})
+            }
+                      
+        })
+    }
 
     module.exports={
         getAllStudentData:getAllStudentData,
         getStudentByID:getStudentByID,
-        insertStudent:insertStudent
-    
-    }
+        insertStudent:insertStudent,
+        updateStudent:updateStudent,
+        deleteStudent:deleteStudent  
+        }
     
 
-}
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,11 +1,14 @@
 var express=require('express');
 let router=express.Router();
+const validate=require('../Validation/student.validation')
 
 let studentController=require('../Controller/student.controller');
 
 router.get('/showAllStudent',studentController.getAllStudentData);
-router.post('/searchStudentById/:id',studentController.getStudentByID);
-router.post('/insertStudent',studentController.insertStudent);
+router.post('/findStudentById/:id',studentController.getStudentByID);
+router.post('/insertStudent', validate.validate,studentController.insertStudent);
+router.post('/updateStudent',studentController.updateStudent);
+router.post('/deleteStudent/:id',studentController.deleteStudent);
 
 
 module.exports=router;
